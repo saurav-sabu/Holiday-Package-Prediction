00c8050808f877c4e7075bbd3fac9f5ab369a80c
Saurav Sabu <saurav.sabu9@gmail.com>
2024-08-04 17:31:32 +0530

    Added comments for setup file

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup, find_packages
 
-
+# Setup function to handle package metadata and configuration
 setup(
     name="Holiday-Package-Prediction",
     version="0.0.0",
     author="Saurav Sabu",
     author_email="saurav.sabu9@gmail.com",
-    packages=find_packages()
+    packages=find_packages()  # Automatically find and include all packages in the directory
 )
