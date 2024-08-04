9e968472c53825bada6f5b129eccc49457295a47
Saurav Sabu <saurav.sabu9@gmail.com>
2024-08-04 17:30:50 +0530

    Added comments in template file

--- template.py
+++ template.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pathlib import Path
 
-
+# List of files to be created
 list_of_files = [
     "requirements.txt",
     "setup.py",
@@ -23,18 +23,22 @@ list_of_files = [
     "src/exception.py"
 ]
 
+# Loop through each file path in the list
 for file_path in list_of_files:
-    file_path = Path(file_path)
+    file_path = Path(file_path) # Convert to a Path object for easier manipulation
     
-    file_dir,file_name = os.path.split(file_path)
+    file_dir,file_name = os.path.split(file_path) # Split into directory and file name
 
+    # Create the directory if it does not exist
     if file_dir != "":
         os.makedirs(file_dir,exist_ok=True)
 
+    
+    # Create the file if it does not exist or if it is empty
     if (not os.path.exists(file_path)) or (os.path.getsize(file_path) ==0):
         with open(file_path,"w") as f:
-            pass
+            pass # Create an empty file
     else:
-        print(f"File already exists: {file_path}")
+        print(f"File already exists: {file_path}") # Print a message if the file already exists
 
     
